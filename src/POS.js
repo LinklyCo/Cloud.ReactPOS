a05acb14434dfa6a88acbdf0b8f0fdec3fadcbd0
Peter Fallon <pfallon@linkly.com.au>
2020-10-29 16:55:29 +1100

    reprint receipt and txn status functions + some refactoring

--- src/POS.js
+++ src/POS.js
@@ -7,11 +7,16 @@ import { retrieveLinklyData, storeLinklyData } from "./SecretManager";
 
 const PAIRING_MENU = "PAIRING_MENU";
 const POS_SALE_UI = "POS_SALE_UI";
+const TXN = "TXN";
+const REPRINT_RECEIPT = "REPRINT_RECEIPT";
+const TXN_STATUS = "TXN_STATUS";
 
 const pairingPath =
   "https://auth.sandbox.cloud.pceftpos.com/v1/pairing/cloudpos";
 const tokenRequestPath =
   "https://auth.sandbox.cloud.pceftpos.com/v1/tokens/cloudpos";
+const linklyEndpoint =
+  "https://rest.pos.sandbox.cloud.pceftpos.com/v1/sessions/";
 const posName = "React REST POS";
 const posVersion = "1.0.0";
 const posId = "f818d27b-c98c-4007-97f5-6eb173bb7d9b";
@@ -32,6 +37,7 @@ const POS = (props) => {
   const [paired, setPaired] = useState(false);
   const [output, setOutput] = useState("");
   const [txnResponse, setTxnResponse] = useState();
+  const [lastSessionId, setLastSessionId] = useState();
 
   console.log("paired :>> ", paired);
 
@@ -133,30 +139,107 @@ const POS = (props) => {
       });
   };
 
-  const eftpos = (params) => {
+  const purchase = (params) => {
     const request = {
-      txnType: "P",
-      amtPurchase: 100,
-      txnRef: "1123456789ABCDEF",
+      Merchant: "00",
+      TxnType: "P",
+      AmtPurchase: 100,
+      TxnRef: "1234567890",
+      CurrencyCode: "AUD",
+      CutReceipt: "0",
+      ReceiptAutoPrint: "0",
+      Application: "00",
+      PurchaseAnalysisData: {
+        OPR: "00766|test",
+        AMT: "0042000",
+        PCM: "0000",
+      },
+      Basket: {
+        id: "t39kq18134553",
+        amt: 2145,
+        tax: 200,
+        dis: 50,
+        sur: 0,
+        items: [
+          {
+            id: "t39kq002",
+            sku: "k24086723",
+            qty: 2,
+            amt: 2145,
+            tax: 200,
+            dis: 50,
+            name: "XData USB Drive",
+          },
+        ],
+      },
     };
 
-    sendPurchaseRequest(request);
+    sendRequest(request, TXN);
   };
 
-  const aliPay = (params) => {
+  const refund = () => {
+    try {
+      const request = {
+        Merchant: "00",
+        TxnType: "R",
+        AmtPurchase: 100,
+        TxnRef: "1234567890",
+        CurrencyCode: "AUD",
+        CutReceipt: "0",
+        ReceiptAutoPrint: "0",
+        App: "00",
+        PurchaseAnalysisData: {
+          RFN: txnResponse.PurchaseAnalysisData.RFN,
+          OPR: "00766|test",
+          AMT: "0042000",
+          PCM: "0000",
+        },
+      };
+
+      sendRequest(request, TXN);
+    } catch (error) {
+      console.log("error :>> ", error);
+    }
+  };
+
+  const oneButton = (params) => {
     const request = {
       txnType: "P",
       amtPurchase: 100,
       txnRef: "96B32M9UNZ421MEI",
-      merchant: "66",
+      merchant: "99",
       application: "02",
       receiptAutoPrint: "0",
     };
 
-    sendPurchaseRequest(request);
+    sendRequest(request, TXN);
   };
 
-  const sendPurchaseRequest = (request) => {
+  const reprintReceipt = (params) => {
+    try {
+      const request = {
+        Merchant: "99",
+        ReceiptAutoPrint: "0",
+        ReprintType: "2",
+      };
+
+      sendRequest(request, REPRINT_RECEIPT);
+    } catch (error) {
+      console.log("error :>> ", error);
+    }
+  };
+
+  const getTxnStatus = (params) => {
+    try {
+      const request = {};
+
+      sendRequest(request, TXN_STATUS);
+    } catch (error) {
+      console.log("error :>> ", error);
+    }
+  };
+
+  const sendRequest = (request, type) => {
     // Add a request interceptor
     axios.interceptors.request.use(
       (config) => {
@@ -170,20 +253,48 @@ const POS = (props) => {
       }
     );
 
-    const sessionUUID = uuidv4();
+    let tempSessionId = uuidv4();
 
-    const uri =
-      "https://rest.pos.sandbox.cloud.pceftpos.com/v1/sessions/" +
-      sessionUUID +
-      "/transaction?async=false";
+    let requestType;
+    switch (type) {
+      case TXN:
+        requestType = "/transaction?async=false";
+        break;
+      case REPRINT_RECEIPT:
+        requestType = "/reprintreceipt?async=false";
+        break;
+      case TXN_STATUS:
+        requestType = "/transaction?async=false";
+        tempSessionId = lastSessionId;
+        break;
+    }
 
+    const uri = linklyEndpoint + tempSessionId + requestType;
     console.log("request :", request);
+
+    if (type === TXN_STATUS) {
+      return axios
+        .get(uri)
+        .then((response) => {
+          console.log("response :", response);
+          const selectedData = {
+            Status: response.status,
+            Message: response.statusText,
+          };
+          setTxnResponse(selectedData);
+        })
+        .catch((error) => {
+          console.log("ERROR", error);
+          setTxnResponse(error);
+        });
+    }
+
+    setLastSessionId(tempSessionId);
 
     return axios
       .post(
         uri,
-
-        { request: request } //, txnHeaders
+        { request } //, txnHeaders
       )
       .then((response) => {
         console.log("response :", response);
@@ -246,19 +357,37 @@ const POS = (props) => {
           <button
             type="button"
             className="btn btn-outline-info"
-            onClick={eftpos}
+            onClick={purchase}
           >
             Purchase
           </button>
-          <button type="button" className="btn btn-outline-info">
+          <button
+            type="button"
+            className="btn btn-outline-info"
+            onClick={refund}
+          >
             Refund
           </button>
           <button
             type="button"
             className="btn btn-outline-info"
-            onClick={aliPay}
+            onClick={oneButton}
           >
             One Button
+          </button>
+          <button
+            type="button"
+            className="btn btn-outline-info"
+            onClick={reprintReceipt}
+          >
+            Reprint receipt
+          </button>
+          <button
+            type="button"
+            className="btn btn-outline-info"
+            onClick={getTxnStatus}
+          >
+            Txn Status
           </button>
         </div>
       );
